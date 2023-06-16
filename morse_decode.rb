0793ce188b7854d1aa95f2f678b9792061a5848d
Ellyboi <vitoesitemitope@gmail.com>
2023-06-16 21:55:37 +0100

    add decode morse code of message

---
+++ morse_decode.rb
@@ -0,0 +1,66 @@
+def decode_char(letter)
+  case letter
+  when '.-'
+    'A'
+  when '-...'
+    'B'
+  when '-.-.'
+    'C'
+  when '-..'
+    'D'
+  when '.'
+    'E'
+  when '..-.'
+    'F'
+  when '--.'
+    'G'
+  when '....'
+    'H'
+  when '..'
+    'I'
+  when '.---'
+    'J'
+  when '-.-'
+    'K'
+  when '.-..'
+    'L'
+  when '--'
+    'M'
+  when '-.'
+    'N'
+  when '---'
+    'O'
+  when '.--.'
+    'P'
+  when '--.-'
+    'Q'
+  when '.-.'
+    'R'
+  when '...'
+    'S'
+  when '-'
+    'T'
+  when '..-'
+    'U'
+  when '...-'
+    'V'
+  when '.--'
+    'W'
+  when '-..-'
+    'X'
+  when '-.--'
+    'Y'
+  when '--..'
+    'Z'
+  else
+    nil
+  end
+end
+
+def decode_word(code) 
+  code.split(' ').map { |char| decode_char(char) }.join
+end
+
+def decode_message(code)
+  code.split('   ').map { |word| decode_word(word) }.join(' ')
+end
