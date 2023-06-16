7e8a4fff9c88f4c6f27d15938419f13821959f8e
Ellyboi <vitoesitemitope@gmail.com>
2023-06-17 00:05:08 +0100

    update morse code

--- morse_decode.rb
+++ morse_decode.rb
@@ -1,66 +1,33 @@
-def decode_char(letter)
-  case letter
-  when '.-'
-    'A'
-  when '-...'
-    'B'
-  when '-.-.'
-    'C'
-  when '-..'
-    'D'
-  when '.'
-    'E'
-  when '..-.'
-    'F'
-  when '--.'
-    'G'
-  when '....'
-    'H'
-  when '..'
-    'I'
-  when '.---'
-    'J'
-  when '-.-'
-    'K'
-  when '.-..'
-    'L'
-  when '--'
-    'M'
-  when '-.'
-    'N'
-  when '---'
-    'O'
-  when '.--.'
-    'P'
-  when '--.-'
-    'Q'
-  when '.-.'
-    'R'
-  when '...'
-    'S'
-  when '-'
-    'T'
-  when '..-'
-    'U'
-  when '...-'
-    'V'
-  when '.--'
-    'W'
-  when '-..-'
-    'X'
-  when '-.--'
-    'Y'
-  when '--..'
-    'Z'
+morse_characters = {
+  'a' => '.-', 'b' => '-...', 'c' => '-.-.', 'd' => '-..', 'e' => '.',
+  'f' => '..-.', 'g' => '--.', 'h' => '....', 'i' => '..', 'j' => '.---',
+  'k' => '-.-', 'l' => '.-..', 'm' => '--', 'n' => '-.', 'o' => '---',
+  'p' => '.--.', 'q' => '--.-', 'r' => '.-.', 's' => '...', 't' => '-',
+  'u' => '..-', 'v' => '...-', 'w' => '.--', 'x' => '-..-', 'y' => '-.--', 'z' => '--..',
+  '1' => '.----', '2' => '..---', '3' => '...--', '4' => '....-', '5' => '.....',
+  '6' => '-....', '7' => '--...', '8' => '---..', '9' => '----.', '0' => '-----'
+}
+
+def decode_char(letter, array)
+  array.map do |key, val|
+    return key.upcase if val == letter
   end
 end
 
-def decode_word(code)
-  code.split.map { |char| decode_char(char) }.join
+puts decode_char('.-', morse_characters)
+
+def decode_word(word, array)
+  word.split.map do |letter|
+    decode_char(letter, array)
+  end.join
 end
 
-def decode_message(code)
-  code.split('   ').map { |word| decode_word(word) }.join(' ')
+puts decode_word('-- -.--', morse_characters)
+
+def decode(message, morse_characters)
+  message.split('   ').map do |word|
+    decode_word(word, morse_characters)
+  end.join(' ')
 end
 
 puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...', morse_characters)
